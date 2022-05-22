a343bfa3d30a3fa71b5cff6a4dbee3a81c8bb1d4
sagar kadam <misc.sagarkadam@gmail.com>
2022-05-22 12:28:01 +0530

    Initial Commit

---
+++ Bulls Eye/Bulls_EyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bulls_EyeApp.swift
+//  Bulls Eye
+//
+//  Created by Sagar Kadam on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Bulls_EyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bulls Eye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bulls Eye
+//
+//  Created by Sagar Kadam on 22/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
