2a80662e6644905af2c54a8917ea1da27c769f98
sagar kadam <misc.sagarkadam@gmail.com>
2022-05-24 23:15:04 +0530

    Code Update : Core Concept "SFSymbols".
    Why ? & What I learned ::-
    1. Updated the arrow to view in regular and dark mode using the TextColor

--- Bulls Eye/Views/RoundViews.swift
+++ Bulls Eye/Views/RoundViews.swift
@@ -11,12 +11,22 @@ struct RoundedImageViewStroked: View {
     var systemName: String
     var body: some View {
         Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("TextColor"))
+            .frame(width: 56.0, height: 56.0)
+    }
+}
 
+struct PreviewView: View {
+    var body: some View {
+        RoundedImageViewStroked(systemName: "arrow.counterclockwise")
     }
 }
 
 struct RoundViews_Previews: PreviewProvider {
     static var previews: some View {
-        RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+        PreviewView()
+        PreviewView()
+            .preferredColorScheme(.dark)
     }
 }
