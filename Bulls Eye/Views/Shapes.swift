bb684519e848cea2eaf206a9d827595c40bb2747
sagar kadam <misc.sagarkadam@gmail.com>
2022-05-24 10:55:39 +0530

    Code Update : Core Concept "Fill & Strokes".
    Why ? & What I learned ::-
    1. How we can add the different types of shapes in view and apply the fill and other modifiers on the same.

---
+++ Bulls Eye/Views/Shapes.swift
@@ -0,0 +1,39 @@
+//
+//  Shapes.swift
+//  Bulls Eye
+//
+//  Created by Sagar Kadam on 24/05/22.
+//
+
+import SwiftUI
+
+struct Shapes: View {
+    var body: some View {
+        VStack {
+            Circle()
+                .inset(by: 10)
+                .stroke(.blue, lineWidth: 20)
+                .frame(width: 200, height: 100)
+            
+            RoundedRectangle(cornerRadius: 20.0)
+                .fill(Color.orange)
+                .frame(width: 200, height: 100)
+
+            Capsule()
+                .fill(Color.red)
+                .frame(width: 200, height: 100)
+            
+            Ellipse()
+                .fill(Color.yellow)
+                .frame(width: 200, height: 100)
+            
+        }
+        .background(Color.green)
+    }
+}
+
+struct Shapes_Previews: PreviewProvider {
+    static var previews: some View {
+        Shapes()
+    }
+}
