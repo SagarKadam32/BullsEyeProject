2b975fdfdf1c0992978217f2c9198e585455d0f6
sagar kadam <misc.sagarkadam@gmail.com>
2022-05-25 09:18:46 +0530

    Code Update : Core Concept "SFSymbols".
    Why ? & What I learned ::-
    1. Updated background and foreground colour with addition of overlay and background modifiers.
    2. Added more color properties for the button enhancement.

--- Bulls Eye/Views/RoundViews.swift
+++ Bulls Eye/Views/RoundViews.swift
@@ -14,12 +14,34 @@ struct RoundedImageViewStroked: View {
             .font(.title)
             .foregroundColor(Color("TextColor"))
             .frame(width: 56.0, height: 56.0)
+            .overlay(
+                    Circle()
+                    .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2)
+                    .frame(width: 56, height: 56)
+            )
+    }
+}
+
+struct RoundedImageViewFilled: View {
+    var systemName: String
+    var body: some View {
+        Image(systemName: systemName)
+            .font(.title)
+            .foregroundColor(Color("ButtonFilledTextColor"))
+            .frame(width: 56.0, height: 56.0)
+            .background(
+                Circle()
+                .fill(Color("ButtonFilledBackgroundColor"))
+            )
     }
 }
 
 struct PreviewView: View {
     var body: some View {
-        RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+        VStack(spacing: 10) {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            RoundedImageViewFilled(systemName: "list.dash")
+        }
     }
 }
 
