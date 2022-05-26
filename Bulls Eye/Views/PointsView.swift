cad88f841fcd9d0702907234180daeb15be8c6dd
sagar kadam <misc.sagarkadam@gmail.com>
2022-05-26 10:31:37 +0530

    Code Update : Challenge: Create a Custom Alert Try creating the custom alert itself, by using the building blocks we created earlier.
    Why ? & What I learned ::-
    1. Updated the Points View to display the slider view text message correctly.

--- Bulls Eye/Views/PointsView.swift
+++ Bulls Eye/Views/PointsView.swift
@@ -8,41 +8,43 @@
 import SwiftUI
 
 struct PointsView: View {
-    @Binding var alertIsVisible: Bool
-    @Binding var sliderValue: Double
-    @Binding var game: Game
+//    @Binding var alertIsVisible: Bool
+//    @Binding var sliderValue: Double
+//    @Binding var game: Game
     
     var body: some View {
-        ZStack {
-            Color.gray
+        VStack(spacing:10) {
+//            let roundedVar = Int(sliderValue.rounded())
+            InstructionText(textInstruction: "Your selected slider value is")
+            BigNumberText(textInput: "99")
+            ButtonText(textInput: "Start New Round".uppercased())
+
+//            Button(action: {
+//                game.startNewRound(points: game.point(sliderValue: Int(sliderValue)))
+//            }){
+//                ButtonText(textInput: "Start New Round".uppercased())
+//            }
+        }
                 .padding()
                 .frame(maxWidth: 300)
                 .background(Color("BackgroundColor"))
                 .cornerRadius(21.0)
             .shadow(radius: 10, x: 5, y: 5)
             
-            VStack(spacing:10) {
-                let roundedVar = Int(sliderValue.rounded())
-                InstructionText(textInstruction: "\nOriginal Target Value = \(game.target) \n\n Your selected slider value is \(roundedVar).\n" +
-                                "Your current score is \(game.point(sliderValue: roundedVar)) in this round.")
-                BigNumberText(textInput: String(roundedVar))
-                Button(action: {
-                    game.startNewRound(points: game.point(sliderValue: Int(sliderValue)))
-                }){
-                    ButtonText(textInput: "Start New Round".uppercased())
-                }
-            }
+          
+        
+        
             
-        }
+        
     }
 }
 
-//struct PointsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        PointsView()
-//            .environment(\.sizeCategory, .large)
-//        PointsView().previewLayout(.fixed(width: 568, height: 320))
-//
-//        PointsView().preferredColorScheme(.dark)
-//        PointsView().previewLayout(.fixed(width: 568, height: 320)).preferredColorScheme(.dark)    }
-//}
+struct PointsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PointsView()
+            .environment(\.sizeCategory, .large)
+        PointsView().previewLayout(.fixed(width: 568, height: 320))
+
+        PointsView().preferredColorScheme(.dark)
+        PointsView().previewLayout(.fixed(width: 568, height: 320)).preferredColorScheme(.dark)    }
+}
